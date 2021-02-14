9798b5e1c12379ee9805c31e4f33404a79a1a3a6
Farjana Sultana Samia <73123239+Samia-00@users.noreply.github.com>
2021-02-14 14:04:29 +0600

    Finding Age

---
+++ Age_Count/Date_Tiime/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Date_Tiime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dob = DateTime.Parse(textBox2.Text);
+            int age = DateTime.Now.Subtract(dob).Days / 365;
+            textBox3.Text =
+            " You  are  " + age + "  years  old!!! ";
+        }
+    }
+}

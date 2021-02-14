f05b97d82bc3cf955f0da8cd2459692e84890ced
Farjana Sultana Samia <73123239+Samia-00@users.noreply.github.com>
2021-02-14 13:55:18 +0600

    Project 2

---
+++ project 1/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello bangladesh!!!!!!!!!!!!!!");
+            int firstNumber=10;
+            int secondNuber=20;
+            string something;
+            int luckyNumb = 5;
+            long totalCount = 103;
+            short abc = 10;
+
+            Console.Write("Hello" + Environment.NewLine);
+            Console.WriteLine("Total Number : " + (firstNumber + secondNuber));
+            Console.ReadKey();
+           // Console.ReadLine();
+        }
+    }
+}
---
+++ project 1/secondData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fraction data type
+            float rate = 2.75F;
+            double basic = 9800.00;
+            decimal expanse = 897.23M;
+
+            Console.WriteLine("Rate : " + rate);
+            Console.WriteLine("Basic : " + basic);
+            Console.WriteLine("Expanse : " +expanse);
+
+            Console.ReadKey();
+        }
+    }
+}

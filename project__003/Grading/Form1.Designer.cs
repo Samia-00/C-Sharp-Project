a56c6c61b3d254fc7face8a1e0c7777803773c32
Farjana Sultana Samia <73123239+Samia-00@users.noreply.github.com>
2021-02-14 13:52:33 +0600

    Grading sheet design

---
+++ project__003/Grading/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Grading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Stencil", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(360, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(666, 58);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter Your Grade";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text
+            // 
+            this.text.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.Location = new System.Drawing.Point(541, 244);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(319, 55);
+            this.text.TabIndex = 1;
+            this.text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.text.TextChanged += new System.EventHandler(this.text_TextChanged);
+            // 
+            // text2
+            // 
+            this.text2.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text2.Location = new System.Drawing.Point(426, 379);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(559, 55);
+            this.text2.TabIndex = 2;
+            this.text2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(28F, 51F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1684, 1035);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Stencil", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.Name = "Form1";
+            this.Text = "Grading";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.TextBox text2;
+    }
+}
+
---
+++ project__003/Grading/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+
+            any();
+        }
+        void any()
+        {
+            string grade = Console.ReadLine();
+            //char grade;
+           // grade=Convert.ToChar(Console.ReadLine());
+
+            switch (ToString(grade))
+            {
+                case "A":
+                    text2.Text = "Excelent!!!!!!!!!!!!";
+                    break;
+                case "B":
+                case "C":
+                    text2.Text = "Well Done !!!!!!!!!!!!";
+                    break;
+                case "D":
+                    text2.Text = "You Passed!!!!!!!!!!!!";
+                    break;
+                case "F":
+                    text2.Text = "Better Luck Next Time!!!!!!!!!!!!!!!!!!!!!!!";
+                    break;
+                default:
+                    text2.Text = "INVALID";
+                    break;
+            }
+        }
+    }
+}

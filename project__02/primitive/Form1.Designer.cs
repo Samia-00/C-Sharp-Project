5282b1a1973666961a5c98ebece5b164f6a694fe
Farjana Sultana Samia <73123239+Samia-00@users.noreply.github.com>
2021-02-14 13:51:01 +0600

    Primitive project

---
+++ project__02/primitive/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace primitive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(313, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(656, 61);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Primitive Data Type";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Khaki;
+            this.textBox2.Location = new System.Drawing.Point(88, 114);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 52);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Choose A Data Type";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Khaki;
+            this.textBox3.Location = new System.Drawing.Point(718, 114);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(8);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(400, 52);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Sample Value";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text
+            // 
+            this.text.BackColor = System.Drawing.Color.Khaki;
+            this.text.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.Location = new System.Drawing.Point(693, 214);
+            this.text.Margin = new System.Windows.Forms.Padding(8);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(470, 44);
+            this.text.TabIndex = 3;
+            this.text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Khaki;
+            this.listBox1.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Items.AddRange(new object[] {
+            "int",
+            "",
+            "long",
+            "",
+            "float",
+            "",
+            "double",
+            "",
+            "decimal",
+            "",
+            "string",
+            "",
+            "char",
+            "",
+            "bool"});
+            this.listBox1.Location = new System.Drawing.Point(88, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 559);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Goldenrod;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(812, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 61);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Name = "Form1";
+            this.Text = "Primitive Data Type";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ project__02/primitive/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primitive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    text.Text = "14";
+                    break;
+                case 1:
+                    text.Text = " ";
+                    break;
+                case 2:
+                    text.Text = "14L";
+                    break;
+                case 3:
+                    text.Text = " ";
+                    break;
+                case 4:
+                    text.Text = "14.5F";
+                    break;
+                case 5:
+                    text.Text = " ";
+                    break;
+                case 6:
+                    text.Text = "14.256882838";
+                    break;
+                case 7:
+                    text.Text = " ";
+                    break;
+                case 8:
+                    text.Text = "3.003764834333M";
+                    break;
+                case 9:
+                    text.Text = " ";
+                    break;
+                case 10:
+                    text.Text = "Farjana Sultana Samia";
+                    break;
+                case 11:
+                    text.Text = " ";
+                    break;
+                case 12:
+                    text.Text = "F";
+                    break;
+                case 13:
+                    text.Text = " ";
+                    break;
+                default:
+                    text.Text = "False";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
